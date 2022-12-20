89b0248a48bd452fa22aeffb9ca3e3bc8bc12236
Ronaldo Amaral <ronaldo.amaral@gsuite.iff.edu.br>
2022-12-20 01:30:33 -0300

    Implementação Modelo

--- WebDev-1/src/main/java/br/edu/iff/ccc/bsi/webdev/entities/User.java
+++ WebDev-1/src/main/java/br/edu/iff/ccc/bsi/webdev/entities/User.java
@@ -1,5 +1,44 @@
 package br.edu.iff.ccc.bsi.webdev.entities;
 
 public class User {
-teste
+	public String name;
+	public String lastaname;
+	public String username;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the lastaname
+	 */
+	public String getLastaname() {
+		return lastaname;
+	}
+	/**
+	 * @param lastaname the lastaname to set
+	 */
+	public void setLastaname(String lastaname) {
+		this.lastaname = lastaname;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 }
